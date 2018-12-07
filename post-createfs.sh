3364a3ecc4a3816466b3e5ecc9e5b9bd764e8248
Jaremy Creechley <creechley@gmail.com>
2018-12-07 16:44:58 -0700

    moving files around

--- post-createfs.sh
+++ post-createfs.sh
@@ -6,7 +6,7 @@ FWUP_CONFIG=$NERVES_DEFCONFIG_DIR/fwup.conf
 
 # Create/copy u-boot files to the images directory
 $HOST_DIR/usr/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 \
-    -n "nerves boot script" -d $NERVES_DEFCONFIG_DIR/uboot-script.cmd \
+    -n "nerves boot script" -d $NERVES_DEFCONFIG_DIR/uboot/uboot-script.cmd \
     $BINARIES_DIR/boot.scr
 
 cp $HOST_DIR/../build/uboot-2018.07/u-boot-sunxi-with-spl.bin $BINARIES_DIR

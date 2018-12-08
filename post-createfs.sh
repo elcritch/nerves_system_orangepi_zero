dc9b0470775a1db9b5df19a668e0abd96fd358dd
Jaremy Creechley <creechley@gmail.com>
2018-12-07 22:32:12 -0700

    updates

--- post-createfs.sh
+++ post-createfs.sh
@@ -5,11 +5,12 @@ set -e
 FWUP_CONFIG=$NERVES_DEFCONFIG_DIR/fwup.conf
 
 # Create/copy u-boot files to the images directory
-$HOST_DIR/usr/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 \
-    -n "nerves boot script" -d $NERVES_DEFCONFIG_DIR/uboot/uboot-script.cmd \
-    $BINARIES_DIR/boot.scr
+# $HOST_DIR/usr/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 \
+    # -n "nerves boot script" -d $NERVES_DEFCONFIG_DIR/uboot/uboot-script.cmd \
+    # $BINARIES_DIR/boot.scr
 
 cp $HOST_DIR/../build/uboot-2018.07/u-boot-sunxi-with-spl.bin $BINARIES_DIR
 
 # Run the common post-image processing for nerves
 $BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
+

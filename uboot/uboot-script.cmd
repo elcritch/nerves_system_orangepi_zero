8cd1d9189414a8e899769ce8ab23ff82f1f77be8
Jaremy Creechley <creechley@gmail.com>
2018-12-07 20:34:24 -0700

    cleaning up uboot-script.cmd

--- uboot/uboot-script.cmd
+++ uboot/uboot-script.cmd
@@ -18,68 +18,6 @@
 # See U-Boot/include/configs/ti_armv7_common.h and
 # U-Boot/include/configs/am335x_evm.h for most of what's below.
 
-#
-# Nerves variables
-#
-
-nerves_fw_active=a
-
-# nerves_fw_autovalidate controls whether updates are considered valid once
-# applied. If set to 0, the user needs to set nerves_fw_validated to 1 in their
-# application. If they don't set it before a reboot, then the previous software
-# is run. If 1, then no further action needs to be taken.
-nerves_fw_autovalidate=1
-
-# nerves_fw_validated is 1 if the current boot selection is accepted It is set
-# to 1 here, since this environment is written in the factory, so it is
-# implicitly valid.
-nerves_fw_validated=1
-
-# nerves_fw_booted is 0 for the first boot and 1 for all reboots after that.
-# NOTE: Keep this '0' so that all new boards run a 'saveenv' to exercise the
-#       code that writes back to the eMMC early on.
-nerves_fw_booted=0
-
-# The nerves initialization logic
-#
-# The nerves_init code is run at boot (see the last line of the file). It
-# checks whether this is a first boot or not. If it's not the first boot, then
-# the firmware better be validated or it reverts to running the firmware on
-# the opposite partition.
-nerves_revert=\
-    if test ${nerves_fw_active} = "a"; then\
-        echo "Reverting to partition B";\
-        setenv nerves_fw_active "b";\
-    else\
-        echo "Reverting to partition A";\
-        setenv nerves_fw_active "a";\
-    fi
-
-nerves_init=\
-    if test ${nerves_fw_booted} = 1; then\
-        if test ${nerves_fw_validated} = 0; then\
-            run nerves_revert;\
-            setenv nerves_fw_validated 1;\
-            saveenv;\
-        fi;\
-    else\
-        setenv nerves_fw_booted 1;\
-        if test ${nerves_fw_autovalidate} = 1; then\
-            setenv nerves_fw_validated 1;\
-        fi;\
-        saveenv;\
-    fi;\
-    setenv bootfile zImage.${nerves_fw_active};\
-    if test ${nerves_fw_active} = "a"; then\
-        setenv uenv_root /dev/mmcblk0p2;\
-        setenv bootpart 0:2;\
-    else\
-        setenv uenv_root /dev/mmcblk0p3;\
-        setenv bootpart 0:3;\
-    fi
-
-# Custom U-Boot startup script for Nerves
-
 echo "Running Nerves U-Boot script"
 
 # =========================================================================== #
@@ -87,7 +25,6 @@ echo "Running Nerves U-Boot script"
 # =========================================================================== #
 
 # Defaults
-console=ttyS0,115200n8
 bootdir=
 fdtdir=/boot
 fdtfile=undefined
@@ -96,14 +33,10 @@ devtype=mmc
 kernel_bootpart=0:1
 
 # default values
-setenv load_addr "0x44000000"
-setenv verbosity "1"
-setenv console "both"
-setenv disp_mem_reserves "off"
-setenv rootfstype "squashfs"
-
-## Armbian:
-# setenv rootdev "/dev/mmcblk0p1"
+load_addr="0x44000000"
+verbosity="1"
+disp_mem_reserves="off"
+rootfstype="squashfs"
 
 ## Nerves BBB:
 # Note the root filesystem specification. In Linux, /dev/mmcblk0 is always
@@ -111,7 +44,7 @@ setenv rootfstype "squashfs"
 # Therefore, we hardcode root=/dev/mmcblk0p2 since we always want to mount
 # the root partition off the same device that ran u-boot and supplied
 # zImage.
-setenv rootdev "/dev/mmcblk0p2"
+rootdev="/dev/mmcblk0p2"
 
 # =========================================================================== #
 # Boot Runtime Configuration
@@ -120,9 +53,6 @@ setenv rootdev "/dev/mmcblk0p2"
 # Print boot source
 itest.b *0x28 == 0x00 && echo "Booting from SD"
 itest.b *0x28 == 0x02 && echo "Booting from eMMC or secondary SD"
-
-if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=tty1"; fi
-if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} console=ttyS0,115200"; fi
 
 # # Allow the user to override the kernel/erlinit arguments
 # # via a "uEnv.txt" file in the FAT partition.
@@ -140,8 +70,6 @@ if test "${console}" = "serial" || test "${console}" = "both"; then setenv conso
 # =========================================================================== #
 # Boot System
 # =========================================================================== #
-
-# setenv bootargs "root=${rootdev} ro rootwait rootfstype=squashfs ${consoleargs} cgroup_enable=memory panic=10 consoleblank=0 enforcing=0 loglevel=${verbosity}"
 
 # Disable GPU memory (?)
 # if test "${disp_mem_reserves}" = "off"; then setenv bootargs "${bootargs} sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_fb_mem_reserve=16"; fi
